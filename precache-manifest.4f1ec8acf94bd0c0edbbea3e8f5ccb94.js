5057e3f91ea1193b6db7bf3fa62ef818764ddff8
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-17 17:34:12 +0300

    Updates

---
+++ precache-manifest.4f1ec8acf94bd0c0edbbea3e8f5ccb94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d2be71bdaa4faa3e57371240bee045",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aecd06689c1f3faa6780",
+    "url": "./static/css/main.bf243b1a.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecd06689c1f3faa6780",
+    "url": "./static/js/main.2fce2128.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
